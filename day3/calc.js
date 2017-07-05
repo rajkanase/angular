864295dd26ec59e1d0ca3b9735d377887b12c4e6
Rajkumar Kanase <rajkanase11@gmail.com>
2017-07-05 09:31:02 +0530

    calculator

---
+++ day3/calc.js
@@ -0,0 +1,13 @@
+let add = function (a,b) {
+    console.log(`Addition is ${a+b}`)
+}
+
+let sub = (a,b) => {
+    console.log(`Substraction is ${a-b}`)
+}
+
+let mul = (a,b) => {console.log(`Multiplicatin is ${a*b}`)}
+
+add(10,20)
+sub(30,20)
+mul(10,10)
